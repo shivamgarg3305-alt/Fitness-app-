e7167fe52790306c668befaa9a7158650334039c
shivamgarg3305-alt <shivamgarg3305@gmail.com>
2026-09-14 15:10:34 +0530

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color navyDeep = Color(0xFF0B132B);
+
+  static const Color cyan = Color(0xFF00D9FF);
+  static const Color cyanBright = Color(0xFF5CEBFF);
+
+  static const Color textPrimary = Color(0xFFF5F7FA);
+  static const Color textSecondary = Color(0xFFB8C2D1);
+  static const Color textDim = Color(0xFF7F8A9A);
+
+  static const Color micGreen = Color(0xFF32D74B);
+
+  static const LinearGradient dashboardBackground = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      Color(0xFF101C3A),
+      Color(0xFF0B132B),
+    ],
+  );
+
+  static const LinearGradient sessionButtonGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color(0xFF00D9FF),
+      Color(0xFF007AFF),
+    ],
+  );
+}
