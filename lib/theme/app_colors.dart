e6beb04ab76d3954c822e1249525657195e3153a
shivamgarg3305-alt <shivamgarg3305@gmail.com>
2026-09-14 10:35:02 +0000

    Rewrite app_colors.dart with clean deduplicated members

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,36 +1,7 @@
+
 import 'package:flutter/material.dart';
 
 class AppColors {
-
-  static const Color navyDeep = Color(0xFF0B132B);
-
-  static const Color cyan = Color(0xFF00D9FF);
-  static const Color cyanBright = Color(0xFF5CEBFF);
-
-  static const Color textPrimary = Color(0xFFF5F7FA);
-  static const Color textSecondary = Color(0xFFB8C2D1);
-  static const Color textDim = Color(0xFF7F8A9A);
-
-  static const Color micGreen = Color(0xFF32D74B);
-
-  static const LinearGradient dashboardBackground = LinearGradient(
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    colors: [
-      Color(0xFF101C3A),
-      Color(0xFF0B132B),
-    ],
-  );
-
-  static const LinearGradient sessionButtonGradient = LinearGradient(
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-    colors: [
-      Color(0xFF00D9FF),
-      Color(0xFF007AFF),
-    ],
-  );
-
   // Base surfaces
   static const Color pureBlack = Color(0xFF000000);
   static const Color navyBase = Color(0xFF0B132B);
@@ -61,5 +32,4 @@ class AppColors {
     Color(0xFF00BCD4),
     Color(0xFF18FFFF),
   ];
-
 }
